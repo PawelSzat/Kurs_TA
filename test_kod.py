8cf372cfc1fa1fbfd2c1652ccb7a61e58378c623
PawelSzat <pawel.szatkowski90@gmail.com>
2024-11-20 22:13:48 +0100

    Update test_kod.py

--- test_kod.py
+++ test_kod.py
@@ -1,41 +1,47 @@
-"""
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
-def test_store_search(selenium):
-    selenium.get("https://kodilla.com/pl/test/store")
-    search = WebDriverWait(selenium, 60).until(
-        lambda s: s.find_element(By.NAME, "search")
-    )
-    search.send_keys("School")
-    time.sleep(3)
-    selenium.quit()
-"""
-#musialem zmienic nazwe pliku ze wzgledu na problem z uprawnieniami
 import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
-def test_ebay_search(selenium):
-    
-    selenium.get("https://www.ebay.pl/")
 
-    search_field = WebDriverWait(selenium, 60).until(
+def search_on_ebay(selenium, search_term):
+    """
+    Wyszukaj podany termin na stronie eBay.
+
+    Args:
+      selenium: Obiekt sterownika Selenium.
+      search_term: Termin do wyszukania.
+    """
+    search_field = WebDriverWait(selenium, 10).until(
         lambda s: s.find_element(By.NAME, "_nkw")
     )
-
-    search_field.send_keys("Laptop")
-
+    search_field.send_keys(search_term)
     search_field.send_keys(Keys.ENTER)
+    time.sleep(3)  # Odczekaj na załadowanie wyników
 
-    time.sleep(3)
 
+def verify_category_link(selenium, category_name):
+    """
+    Sprawdź, czy link do kategorii jest wyświetlany.
+
+    Args:
+      selenium: Obiekt sterownika Selenium.
+      category_name: Nazwa kategorii do wyszukania.
+    """
     category_link = WebDriverWait(selenium, 10).until(
-        lambda s: s.find_element(By.LINK_TEXT, "Laptopy i netbooki")
+        lambda s: s.find_element(By.LINK_TEXT, category_name)
     )
     assert category_link.is_displayed()
+
+
+def test_ebay_search(selenium):
+    """
+    Test wyszukiwania na stronie eBay.
+    """
+    selenium.get("https://www.ebay.pl/")
+
+    search_on_ebay(selenium, "Laptop")
+    verify_category_link(selenium, "Laptopy i netbooki")
 
     selenium.quit()
